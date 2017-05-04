a39ff241b81710f2f204988898d45c6d1486523c
MattyK14 <xmattyk14x@hotmail.com>
2017-05-04 16:48:41 -0230

    Add createUser function

--- firebase-auth/functions/create_user.js
+++ firebase-auth/functions/create_user.js
@@ -1,3 +1,20 @@
+const admin = require('firebase-admin');
+
 module.exports = (request, response) => {
-  response.send(request.body);
+  const body = JSON.parse(request.body);
+  //  Verify the user provided a phone number
+  if (!body.phoneNumber) {
+    return response.status(422).send({ error: 'Bad Input' });
+  }
+
+  //  Format the phone number to remove '-' & '()'
+  const phoneNumber = String(body.phoneNumber).replace(/[^\d]/g, '');
+
+  //  Create a new user with said phone number
+  admin.auth().createUser({ uid: phoneNumber })
+    .then(user => response.send(user))
+    .catch(error => response.status(422).send({ error }));
+
+  //  Confirm account was made with the user
+  //(709) 701-0730
 };
--- firebase-auth/functions/index.js
+++ firebase-auth/functions/index.js
@@ -1,6 +1,7 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
+const serviceAccount = require('./service_account.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
